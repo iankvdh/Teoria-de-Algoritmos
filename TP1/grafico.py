d8ed4c981de2c0a4c0def826668e6bacd5827640
juandelaHD <jdelacruz@fi.uba.ar>
2024-10-13 15:10:50 -0300

    [PERF] Mejora en ejecución de graficos. Modularizamos la funcion de gráficos para que no se ejecuten siempre los juegos de los archivos dados en la carpeta data.

--- TP1/grafico.py
+++ TP1/grafico.py
@@ -9,12 +9,13 @@ from src.cant_vs_puntos import mostrar_cantidad_vs_puntos
 
 CARPETA = 'data'
 
-def ejecutar_graficos():
+def monedas_desde_archivos():
+    """
+    Lee los archivos de la carpeta data y devuelve una lista con los tamaños
+    de las listas de monedas y otra lista con las listas de monedas.
+    """
     listas = []
     tamaños = []
-    puntos_sofia = []
-    puntos_mateo = []
-
     carpeta_data = CARPETA
     archivos_txt = [f for f in os.listdir(carpeta_data) if f.endswith('.txt')]
 
@@ -22,26 +23,51 @@ def ejecutar_graficos():
         ruta_archivo = os.path.join(carpeta_data, archivo) 
         lista = leer_numeros_desde_txt(archivo)
         listas.append(lista)
-        
+
     listas.sort(key=len)
     for lista in listas:
         tamaños.append(len(lista))
 
+    return tamaños, listas
+
+def jugar_muchos_arreglos(listas):
+    """
+    Dada una lista de listas de monedas, devuelve una lista con los tiempos de ejec
+    de cada una de las listas. Además, devuelve dos listas con los puntos de Sofia
+    y Mateo respectivamente.
+    """
     tiempos = []
+    puntos_sofia = []
+    puntos_mateo = []
 
     for i in listas:
         inicio = time.perf_counter()
-        _, suma_sofia, suma_mateo = jugar(i)
+        dp = jugar(i)
+        #CAMBIAR ESTO
+        suma_sofia = dp[0][-1]
+        suma_mateo = sum(i) - suma_sofia
         puntos_sofia.append(suma_sofia)
         puntos_mateo.append(suma_mateo)
         fin = time.perf_counter()
         tiempos.append( (fin - inicio) * 1000) # milisegundos
 
+    return tiempos, puntos_sofia, puntos_mateo
+
+def ejecutar_graficos():
+    """
+    Función que se encarga de ejecutar los gráficos según los argumentos pasados por
+    consola. Si no se pasan argumentos, se imprime un mensaje de error.
+    """
+
     if len(sys.argv) > 1:
         grafico = sys.argv[2]
         if grafico == "tiempo_vs_cantidad":
+            tamaños, listas = monedas_desde_archivos()
+            tiempos, _, _ = jugar_muchos_arreglos(listas)
             mostrar_cantidad_vs_tiempo(tamaños, tiempos)
         elif grafico == "puntos_vs_cantidad":
+            tamaños, listas = monedas_desde_archivos()
+            _, puntos_sofia, puntos_mateo = jugar_muchos_arreglos(listas)
             mostrar_cantidad_vs_puntos(tamaños, puntos_sofia, puntos_mateo)
         elif grafico == "victorias_sofia":
             if len(sys.argv) > 3:
@@ -51,6 +77,7 @@ def ejecutar_graficos():
                 jugar_n_juegos()
         else:
             print("Opción no válida.")
+
     else:
         print("Por favor, ejecute directamente el archivo {ejecutar_graficos.sh}.")
 
