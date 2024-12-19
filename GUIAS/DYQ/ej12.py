a0ae4b1a97df9b674519d3b2c5eecff1543260d2
Juan Martin de la Cruz <jdelacruz@fi.uba.ar>
2024-12-19 13:01:39 -0300

    [ADD] +EJS

--- GUIAS/DYQ/ej12.py
+++ GUIAS/DYQ/ej12.py
@@ -7,4 +7,18 @@
 # Pista: Pensar primero cómo habría que hacer si el arreglo tuviera 4 elementos ({C1, C2, D1, D2}). 
 # Luego, pensar a partir de allí el caso de 8 elementos, etc… para encontrar el patrón.
 
-# ??? NI IDEA ????
+def alternar(arr):
+    return _alternar(arr, 0, len(arr) - 1)
+
+def _alternar(arr, ini, fin):
+    if fin - ini < 2:
+        return arr
+
+    medio = (ini + fin) // 2
+
+    arr = _alternar(arr, ini, medio)
+    arr = _alternar(arr, medio + 1, fin)
+
+    for i in range(1, medio-ini+2, 2):
+        arr[ini+i], arr[medio+i] = arr[medio+i], arr[ini+i]
+    return arr
